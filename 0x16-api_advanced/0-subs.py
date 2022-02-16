5ca4d96728cd48f66521c75f37388f33191a79c6
CarolinaLopera <carolinalopera08@gmail.com>
2022-02-15 23:53:12 -0500

    Fix 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,7 +6,7 @@ import requests
 def number_of_subscribers(subreddit):
     """Function to return number of suscriptors"""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {'User-Agent': 'Mozilla/5.0'}
+    headers = {'User-Agent': 'linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)'}
     resp = requests.get(url, headers=headers, allow_redirects=False)
     if resp.status_code == 404:
         return 0
